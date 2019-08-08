3547f1f68e5a16146e30afce5e91ed9248a30a9f
ooo-my <53900485+ooo-my@users.noreply.github.com>
2019-08-08 14:35:34 +0200

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# reactfirebase
-Created with CodeSandbox
---
+++ src/components/App/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navigation from "../Navigation";
+import LandingPage from "../Landing";
+import SignInPage from "../SignIn";
+import HomePage from "../Home";
+import AccountPage from "../Account";
+
+import * as ROUTES from "../../constants/routes";
+import { withAuthentication } from "../Session";
+
+const App = () => (
+  <Router>
+    <div>
+      <Navigation />
+
+      <hr />
+
+      <Route exact path={ROUTES.LANDING} component={LandingPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route path={ROUTES.HOME} component={HomePage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+    </div>
+  </Router>
+);
+
+export default withAuthentication(App);
